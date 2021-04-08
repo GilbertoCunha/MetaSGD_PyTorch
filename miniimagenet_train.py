8a61e31287ec028b8e897271afc827e8f491050a
Gilberto Cunha <gcacademic@outlook.pt>
2021-04-08 13:49:21 +0100

    clean code for main cycle

--- miniimagenet_train.py
+++ miniimagenet_train.py
@@ -28,8 +28,10 @@ def evaluate(model, dataset, device, desc="Eval Test"):
         losses.append(loss)
 
     accs = list(map(lambda a: a[-1], all_accs))
+    acc = np.array(accs).mean(axis=0).astype(np.float16)
+    loss = np.array(losses).mean(axis=0).astype(np.float16)
 
-    return accs, losses
+    return acc, loss
 
 
 def main():
@@ -89,8 +91,8 @@ def main():
                              k_query=args.k_qry, batchsz=args.eval_steps, resize=args.imgsz)
 
     print("\nMeta-Training:")
-    best_test_acc = 0
     pruning_factor = 0
+    best_tr_acc, best_val_acc, best_test_acc = 0, 0, 0
     epoch_bar = tqdm(range(args.epoch//10000), desc="Training", total=len(range(args.epoch//10)))
     for epoch in epoch_bar:
         # fetch meta_batchsz num of episode each time
@@ -102,24 +104,14 @@ def main():
 
             # Perform training for each task
             x_spt, y_spt, x_qry, y_qry = x_spt.to(device), y_spt.to(device), x_qry.to(device), y_qry.to(device)
-            model(x_spt, y_spt, x_qry, y_qry)                                          
+            model(x_spt, y_spt, x_qry, y_qry)                                
 
             if total_steps % args.save_summary_steps == 0:  # evaluation
 
-                # Get test metrics
-                te_accs, te_losses = evaluate(model, mini_test, device)
-                te_acc = np.array(te_accs).mean(axis=0).astype(np.float16)
-                te_loss = np.array(te_losses).mean(axis=0).astype(np.float16)
-
-                # Get validation metrics
-                val_accs, val_losses = evaluate(model, mini_val, device, "Eval Val")
-                val_acc = np.array(val_accs).mean(axis=0).astype(np.float16)
-                val_loss = np.array(val_losses).mean(axis=0).astype(np.float16)
-
-                # Get train metrics
-                tr_accs, tr_losses = evaluate(model, mini_train_eval, device, "Eval Train")
-                tr_acc = np.array(tr_accs).mean(axis=0).astype(np.float16)
-                tr_loss = np.array(tr_losses).mean(axis=0).astype(np.float16)
+                # Get evaluation metrics
+                te_acc, te_loss = evaluate(model, mini_test, device)
+                val_acc, val_loss = evaluate(model, mini_val, device, "Eval Val")
+                tr_acc, tr_loss = evaluate(model, mini_train_eval, device, "Eval Train")
 
                 # Update Task tqdm bar
                 task_bar.set_postfix({
@@ -130,22 +122,26 @@ def main():
                 })
                 f.write(f"{total_steps},{tr_loss},{tr_acc},{val_loss},{val_acc},{te_loss},{te_acc}\n")
 
-                if te_acc > best_test_acc:
-                    best_test_acc = te_acc
+                # Update best metrics
+                if val_acc > best_val_acc:
+                    best_val_acc = val_acc
                     pruning_factor = 0
                 else:
                     pruning_factor += 1
+                best_test_acc = max(te_acc, best_test_acc)
+                best_tr_acc = max(tr_acc, best_tr_acc)
 
-                if pruning_factor == args.pruning_factor:
+                if pruning_factor == args.pruning:
                     break
 
                 # Update tqdm 
                 epoch_bar.set_postfix({
                     'best_te_acc': best_test_acc,
+                    'best_val_acc': best_val_acc,
                     'pruning': pruning_factor
                 })
 
-        if pruning_factor == args.pruning_factor:
+        if pruning_factor == args.pruning:
             break
 
     f.close()
@@ -170,7 +166,7 @@ if __name__ == '__main__':
     argparser.add_argument('--kernel_size', type=int, help='size of the convolutional kernels', default=9)
     argparser.add_argument('--eval_steps', type=int, help='number of batches to iterate in test mode', default=500)
     argparser.add_argument('--save_summary_steps', type=int, help='frequence to log model evaluation metrics', default=1000)
-    argparser.add_argument('--pruning_factor', type=int, help='stop the training after this number of evaluations without accuracy increase', default=10)
+    argparser.add_argument('--pruning', type=int, help='stop the training after this number of evaluations without accuracy increase', default=12)
     argparser.add_argument('--verbose', type=int, help='print additional information', default=0)
     argparser.add_argument('--seed', type=int, help='seed for reproducible results', default=42)
 
